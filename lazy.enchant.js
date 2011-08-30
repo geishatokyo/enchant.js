f76e6d65866e1b8d186eb1eb123b913dcc9bbf0b
ymrl <ymrl@ymrl.net>
2011-08-30 13:06:39 +0900

    遅延ロードを行うlazy.enchant.jsを追加

---
+++ lazy.enchant.js
@@ -0,0 +1,22 @@
+/*
+ * lazy.enchant.js
+ *
+ * assetを遅延ロードする。
+ * 内部的にはenchant.Game#loadを呼び出すだけです。
+ * 
+ * 読み込みの完了を補足するためには、game.assets[url]のloadイベントにaddEventlistenerすればたぶん大丈夫です。
+ *
+ */
+
+enchant.lazy = {}
+enchant.lazy.load = function(game,url,callback){
+	game.load(url,callback)
+}
+enchant.Game.prototype.lazyload = function(assets){
+  if (!(assets instanceof Array)) {
+  	assets = Array.prototype.slice.call(arguments);
+  }
+	for(var i=0;i<assets.length;i++){
+		enchant.lazy.load(this,assets[i]);
+	}
+}
